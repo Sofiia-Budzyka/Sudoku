881a7845204b78b75317f0fe338a9f9dc406aa61
Sofiia <sofiya2007budzuka@gmail.com>
2025-12-03 17:15:13 +0200

    board

--- src/Grid/Board.java
+++ src/Grid/Board.java
@@ -1,4 +1,20 @@
 package Grid;
 
 public class Board {
+        private Cell[][] cells;
+        public Board() {
+            cells = new Cell[9][9];
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    cells[i][j] = new Cell(0, false);
+                }
+            }
+        }
+
+        public Cell getCell(int row, int col) {
+            return cells[row][col];
+        }
+        public void setInitialDigit(int row, int col, int value) {
+            cells[row][col] = new Cell(value, true);
+        }
 }
