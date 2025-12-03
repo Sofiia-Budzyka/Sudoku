7fb72643df1af089957b2a592171104ac5601370
Sofiia <sofiya2007budzuka@gmail.com>
2025-12-03 22:34:16 +0200

    col and row

--- src/Grid/View.java
+++ src/Grid/View.java
@@ -2,10 +2,20 @@ package Grid;
 
 public class View {
     public void drawBoard(Board board) {
+        System.out.print("    ");
+        for (int col = 0; col < 9; col++) {
+            if (col > 0 && col % 3 == 0) {
+                System.out.print("  ");
+            }
+            System.out.print((col + 1) + " ");
+        }
+        System.out.println();
+        System.out.println("    -----------------------");
         for (int row = 0; row < 9; row++) {
             if (row > 0 && row % 3 == 0) {
-                System.out.println("------+-------+------");
+                System.out.println("    ------+-------+------");
             }
+            System.out.print((row + 1) + " | ");
             for (int col = 0; col < 9; col++) {
                 if (col > 0 && col % 3 == 0) {
                     System.out.print("| ");
@@ -14,10 +24,11 @@ public class View {
                 int value = cell.getValue();
                 String displayValue = (value == 0) ? "." : String.valueOf(value);
                 System.out.print(displayValue + " ");
-                }
-                System.out.println();
             }
-            System.out.println("--------------------\n");
+            System.out.println();
         }
+        System.out.println("    -----------------------\n");
     }
+}
+
 
